88853227a403baaf01a1af0a8e26f4075568db55
siddharth <siddharthp@netskope.com>
2023-05-08 17:51:27 +0530

    Add user routes with error handling middleware and auth guard

---
+++ middlewares/error.js
@@ -0,0 +1,17 @@
+export class ErrorHandler extends Error {
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+export const ErrorMiddleware = (error, req, res, next) => {
+    console.error(error.message);
+    const message = error.message || "Internal Server Error";
+    const statusCode = error.statusCode || 500;
+
+    res.status(statusCode).json({
+        success: false,
+        message,
+    });
+};
