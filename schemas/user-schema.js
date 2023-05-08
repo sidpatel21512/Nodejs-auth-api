0e529708f7f2afd0321aa2978234a702a3eb1ed9
siddharth <siddharthp@netskope.com>
2023-05-08 14:00:54 +0530

    Remove unnecessary files and code

--- schemas/user-schema.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
-});
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 import express from 'express';
-import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import { initializeDB } from './Database/connection.js';
-import baseRouter from './routers/base-routes.js';
-import authRouter from './routers/auth-routes.js';
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.static(path.resolve() + '/public')); // for making express aware of statically served files.
-app.use(express.urlencoded({ extended: true })); //for reading data through form submission.
 app.use(cookieParser());// for parsing cookies through request headers.
-
-app.use(baseRouter,authRouter);
+app.use(express.json());// for parsing requests an response in json format.
 
 
 app.listen(process.env.PORT, () => {
     console.log('Server started on ', process.env.PORT);
-    initializeDB();
 });
